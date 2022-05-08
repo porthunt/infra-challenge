d9fab2129e08aba4879cd3439a5901df874d5db5
Joao Pedro Portela <jpmoreiraportela@kinly.com>
2022-05-08 19:28:14 -0300

    Removing TRANSACTION_QUEUE reference from settings.py and vars.yml

--- primer-api/app/settings.py
+++ primer-api/app/settings.py
@@ -23,9 +23,6 @@ logger = logging.getLogger("primer-challenge")
 transaction_table = os.getenv(
     "TRANSACTION_TABLE", "porthunt-transaction-challenge"
 )
-transaction_queue = os.getenv(
-    "TRANSACTION_QUEUE", "porthunt-primer-challenge-queue"
-)
 transaction_dlq = os.getenv(
     "TRANSACTION_DLQ", "porthunt-primer-challenge-queue-DLQ"
 )
